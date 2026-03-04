439a26f756351a72e99bda2e980c3a077e0c5035
Emmanuel Ademuyiwa <samadeimmanuel@yahoo.com>
2026-03-04 21:41:29 +0100

    favicon

--- school-central/admin/login.php
+++ school-central/admin/login.php
@@ -33,6 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Central CBT - Admin Login</title>
+    <link rel="icon" href="../../portals/public/images/favicon.ico">
     <style>
         * {
             margin: 0;
