4ee8922f7cde88c26d666338f375f97ac265b757
Emmanuel Ademuyiwa <samadeimmanuel@yahoo.com>
2026-03-26 07:37:43 +0100

    register

---
+++ portals/register.php
@@ -0,0 +1,7 @@
+<?php
+// /home/impactdi/public_html/portals/crash.php
+// OR: /home/impactdi/public_html/portals/register.php
+
+// Simple redirect to the registration page
+header("Location: modules/crash_program/register_crash.php");
+exit();
