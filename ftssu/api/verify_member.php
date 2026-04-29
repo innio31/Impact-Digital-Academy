9b7d7d3d02d7cf4ff5931006ddd074c0a23550a6
Emmanuel Ademuyiwa <samadeimmanuel@yahoo.com>
2026-04-29 09:06:07 +0100

    cors update

--- ftssu/api/verify_member.php
+++ ftssu/api/verify_member.php
@@ -1,7 +1,17 @@
 <?php
-header('Content-Type: application/json');
+// Add these headers at the VERY TOP of the file
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Allow-Credentials: true');
+header('Content-Type: application/json');
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 
 include 'db_connect.php';
 
